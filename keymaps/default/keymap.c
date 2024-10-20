b7ca2b69baa8be3a1dcdef94aaef96a8c27dc2ac
Aiden Olsen <olsenaiden33@gmail.com>
2024-10-20 15:47:36 -0600

    custom maxtrix is now implemented

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -4,15 +4,11 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
-                 KC_P7,   KC_P8,   KC_P9,
-        KC_PENT, KC_P4,   KC_P5,   KC_P6,
-        KC_P0,   KC_P1,   KC_P2,   KC_P3,
-    ),
+    [0] = LAYOUT_ortho_4x3(UG_NEXT, KC_P7, KC_P8, KC_P9, KC_PENT, KC_P4, KC_P5, KC_P6, KC_P0, KC_P1, KC_P2, KC_P3),
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = { ENCODER_CCW_CW(C(KC_MINUS), C(KC_EQUAL)) },
+    [0] = {ENCODER_CCW_CW(UG_PREV, UG_NEXT)},
 };
 #endif
