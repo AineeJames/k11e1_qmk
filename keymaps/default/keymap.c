68f5a8e405d77ed16b0e466d5f2538a3e5d445bf
Aiden Olsen <olsenaiden33@gmail.com>
2024-10-20 19:22:23 -0600

    added btter keybinds

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -3,18 +3,59 @@
 
 #include QMK_KEYBOARD_H
 
+enum custom_keycodes {
+    GIT_DIFF = SAFE_RANGE,
+    GIT_PULL,
+    GIT_STATUS,
+    GIT_COMMIT,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case GIT_DIFF:
+            if (record->event.pressed) {
+                SEND_STRING("git diff");
+            }
+            break;
+        case GIT_PULL:
+            if (record->event.pressed) {
+                SEND_STRING("git pull");
+            }
+            break;
+        case GIT_STATUS:
+            if (record->event.pressed) {
+                SEND_STRING("git status");
+            }
+            break;
+        case GIT_COMMIT:
+            if (record->event.pressed) {
+                SEND_STRING("git commit -m \"\"");
+                tap_code(KC_LEFT);
+            }
+            break;
+    }
+    return true;
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
     [0] = LAYOUT_ortho_4x3(
-        UG_NEXT, KC_7, KC_8, KC_9,
-        KC_ENT,  KC_4, KC_5, KC_6,
-        KC_0,    KC_1, KC_2, KC_3
+        TO(1),      LGUI(S(KC_S)), C(KC_MINUS), C(KC_EQUAL),
+        C(S(KC_T)), C(S(KC_B)),    C(KC_R),     C(KC_T),
+        GIT_DIFF,   GIT_PULL,      GIT_STATUS,  GIT_COMMIT
+    ),
+    [1] = LAYOUT_ortho_4x3(
+        TO(0),   UG_TOGG, UG_PREV, UG_NEXT,
+        UG_SPDU, UG_HUEU, UG_SATU, UG_VALU,
+        UG_SPDD, UG_HUED, UG_SATD, UG_VALD
     ),
     //clang-format on
 };
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = {ENCODER_CCW_CW(C(KC_MINUS), C(KC_EQUAL))},
+    [0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
+    [1] = {ENCODER_CCW_CW(UG_PREV, UG_NEXT)},
 };
 #endif
+
--- keymaps/via/keymap.c
+++ keymaps/via/keymap.c
@@ -15,6 +15,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    [0] = {ENCODER_CCW_CW(C(KC_MINUS), C(KC_EQUAL))},
+    [0] = {ENCODER_CCW_CW(UG_HUED, UG_HUEU)},
 };
 #endif
